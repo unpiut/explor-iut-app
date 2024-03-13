b7fec7b9e5a75b55297a403d6ba121c6fc6360e0
Julien Fourdan <julien.fourdan.etu@univ-lemans.fr>
2024-03-13 12:10:00 +0100

    fix bug display

--- src/components/Modale.jsx
+++ src/components/Modale.jsx
@@ -1,13 +1,12 @@
 import React, { useContext } from 'react';
+import { PropTypes } from 'mobx-react';
 import RootStore from '../RootStore';
 import fleche from '../assets/icone-les-iut.svg';
 
 function Modale({ iut }) {
-  console.log(iut._departements[0]);
-  const { iutManager, butManager } = useContext(RootStore);
-  const { _site, _departements } = iut;
+  const { iutManager } = useContext(RootStore);
   function selectionner() {
-    iutManager.iutSelectionnes = iut;
+    iutManager.switchIutSelectionnes(iut);
   }
   function descriptionBut() {
     console.log('yeah');
@@ -15,18 +14,21 @@ function Modale({ iut }) {
   return (
     <div>
       <div className="grid gap-y-2 border-2 text-xs  border-blue-900">
-        <h2 className="align-middle">{_site}</h2>
-        {_departements.map((but) => (
-          <div className="flex w-full justify-around align-middle">
+        <h2 className="align-middle">{iut.site}</h2>
+        {iut.departements.map((but) => (
+          <div className="flex w-full justify-around align-middle" key={but.code}>
             <img width={25} style={{ transform: 'rotate(-0.25turn)' }} src={fleche} alt="fleche" />
             <p>{but.code}</p>
             <button type="button" className="border text-base border-blue-900 px-2 rounded-full" onClick={descriptionBut}>i</button>
           </div>
         ))}
-        <button onClick={selectionner} type="button">{!iutManager.iutSelectionnes.has(iut) ? 'Ajouter cet IUT pour la prise de contact' : 'Retirer cet IUT de la liste de contact'}</button>
+        <button onClick={selectionner} type="button">{!iutManager.iutSelectionnesContains(iut) ? 'Ajouter cet IUT pour la prise de contact' : 'Retirer cet IUT de la liste de contact'}</button>
       </div>
     </div>
   );
 }
+Modale.propTypes = ({
+  iut: PropTypes.objectOrObservableObject.isRequired,
+});
 
 export default Modale;
