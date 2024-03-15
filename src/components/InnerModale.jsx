d4a6d97607e90565eb9272ae4f2ee10baf0ecbdf
Julien Fourdan <julien.fourdan.etu@univ-lemans.fr>
2024-03-15 10:48:06 +0100

    add info of but inside the modale

--- src/components/InnerModale.jsx
+++ src/components/InnerModale.jsx
@@ -1,36 +1,77 @@
 import { PropTypes, observer } from 'mobx-react';
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import RootStore from '../RootStore';
 import fleche from '../assets/icone-les-iut.svg';
 
-function InnerModale({ iut, but }) {
-  const { iutManager } = useContext(RootStore);
+function InnerModale({ iut }) {
+  const { iutManager, butManager } = useContext(RootStore);
+  const [butChoisi, setButChoisi] = useState(null);
+  const bouttonDesc = useRef();
+  const butSelect = butManager.nbButSelectionnes ? butManager.butSelectionnesTab : butManager.buts;
   function selectionner() {
     iutManager.switchIutSelectionnesId(iut);
   }
   function descriptionBut() {
-    console.log('yeah');
+    butManager.getButByCodeWithInfo(bouttonDesc.current.value).then((b) => setButChoisi(b));
+  }
+  const filtre = (b) => (butSelect.find((unBut) => unBut.code === b.codesButDispenses[0]));
+  // const filtreIUT = (d) => (butSelectionnes ? butSelectionnes.has(d) : d); A travailler
+  if (butChoisi) {
+    return (
+      <>
+        <div className="flex justify-around">
+          <button type="button" onClick={() => setButChoisi(null)}>{'<-'}</button>
+          <h2
+            className="align-middle text-base text-center"
+          >
+            {butChoisi.prettyPrintFiliere}
+          </h2>
+        </div>
+        <p>
+          Description formation :
+          {butChoisi.description ? ` ${butChoisi.description}` : ''}
+        </p>
+        <div>
+          <p>
+            Parcours disponible :
+          </p>
+          {butChoisi.parcours.map((parcours) => (
+            <p key={parcours[0]}>
+              {' '}
+              { parcours[1]}
+              {parcours !== butChoisi.parcours[butChoisi.parcours.length - 1] ? ',' : null}
+            </p>
+          ))}
+        </div>
+        <p>
+          Débouchés métiers :
+          {butChoisi.metiers ? ` ${butChoisi.metiers}` : ''}
+        </p>
+        <p>
+          Formation :
+          {` ${butChoisi.nom}`}
+        </p>
+        <div>
+          <a className="underline" target="_blank" href={butChoisi.urlIUT} rel="noreferrer">en savoir plus</a>
+        </div>
+      </>
+    );
   }
   return (
     <>
-      {iut.departements.map((b) => (
-        <div className="flex w-full justify-around align-middle" key={b.code}>
+      {iut.departements.filter(filtre).map((d) => (
+        <div className="flex w-full justify-around align-middle" key={d.code}>
           <img width={25} style={{ transform: 'rotate(-0.25turn)' }} src={fleche} alt="fleche" />
-          <p>{b.code}</p>
-          <button type="button" className="border text-base border-blue-900 px-2 rounded-full" onClick={descriptionBut}>i</button>
+          <p>{d.code}</p>
+          <button type="button" value={d.code} ref={bouttonDesc} className="border text-base border-blue-900 px-2 rounded-full" onClick={descriptionBut}>i</button>
         </div>
       ))}
-      <button onClick={selectionner} type="button">{!iutManager.iutSelectionnesId.has(iut) ? 'Ajouter cet IUT pour la prise de contact' : 'Retirer cet IUT de la liste de contact'}</button>
+      <button onClick={selectionner} type="button">{!iutManager.iutSelectionnesId.has(iut.idIut) ? 'Ajouter cet IUT pour la prise de contact' : 'Retirer cet IUT de la liste de contact'}</button>
     </>
   );
 }
 InnerModale.propTypes = {
-  iut: PropTypes.objectOrObservableObject,
-  but: PropTypes.objectOrObservableObject,
-};
-InnerModale.defaultProps = {
-  iut: null,
-  but: null,
+  iut: PropTypes.objectOrObservableObject.isRequired,
 };
 
 export default observer(InnerModale);
