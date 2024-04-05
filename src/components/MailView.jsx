d371297c7dc010d9e850f791937842664275c16d
Julien Fourdan <julien.fourdan.etu@univ-lemans.fr>
2024-04-05 15:44:43 +0200

    Add file update mail

--- src/components/MailView.jsx
+++ src/components/MailView.jsx
@@ -40,6 +40,12 @@ function MailView() {
             <input type="mail" name="mail" id="mail" autoComplete="mail" value={mailManager.adresseMail} onChange={(evt) => { mailManager.adresseMail = evt.target.value; }} placeholder="mail@mail.com" className="p-1 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </label>
         </div>
+        <div className="m-2">
+          <label htmlFor="alternance" className="block text-sm font-medium leading-6">
+            Offres d&apos;alternance (Non obligatoire)
+            <input type="file" accept=".pdf" multiple name="alternance" id="alternance" className="p-1 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </label>
+        </div>
       </form>
       <Footer
         gauche={{ texte: 'Récapitulatif sélection', lien: 'result' }}
