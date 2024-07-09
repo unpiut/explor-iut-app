23b550828d2fe9fdd9805683c410f3ec07848e48
Hestilien <147434084+Hestilien@users.noreply.github.com>
2024-07-09 10:38:57 +0200

    Update ModifyMailView.jsx

--- src/components/ModifyMailView.jsx
+++ src/components/ModifyMailView.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useContext, useState } from 'react';
 import { observer } from 'mobx-react';
 import { useTranslation } from 'react-i18next';
@@ -9,6 +10,7 @@ function ModifyMailView() {
   const { mailManager, selectedManager, butManager } = useContext(RootStore);
   const [fileNumberState, setfileNumberState] = useState(0);
   const [allFiles, setAllFiles] = useState([null, null, null]);
+  const [textCheck, setTextCheck] = useState([true, true, true, true]);
   const navigate = useNavigate();
   const { t } = useTranslation();
 
@@ -49,6 +51,26 @@ function ModifyMailView() {
     navigate('/mailSend');
   }
 
+  function changeBodyMail(item) {
+    let totalText = '';
+    if (textCheck[0]) {
+      totalText += '- Quelles années sont concernés par l&apos;alternance? \r\n';
+    }
+    if (textCheck[1]) {
+      totalText += '- Quelles sont les plannings d&apos;alternance pour la rentrée prochaine? \r\n';
+    }
+    if (textCheck[2]) {
+      totalText += '- Quelles sont les modalités pour gérer et suivre mon offre d&apos;alternance? \r\n';
+    }
+    if (textCheck[3]) {
+      totalText += '- quelles sont les modalités administratives pour gérer ou suivre mon offre? \r\n';
+    }
+    if (item) {
+      totalText += item.value;
+    }
+    mailManager.corpsMail = totalText;
+  }
+
   return (
     <>
       <h1 className="text-center text-xl lg:text-3xl font-bold">{t('courrielModifTitre')}</h1>
@@ -57,22 +79,44 @@ function ModifyMailView() {
       </p>
       <form method="GET">
         <div className="m-2">
-          <label htmlFor="object" className="block text-sm sm:text-lg font-medium leading-6">
-            {t('courrielModifObjet')}
-            <input type="text" value={mailManager.objet} name="object" id="object" readOnly="readonly" className="block p-1 w-full rounded-md border-0 py-1.5 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-base sm:leading-6" />
-          </label>
-        </div>
-        <div className="m-2">
-          <label htmlFor="contenu" className="block text-sm sm:text-lg font-medium leading-6">
-            {t('courrielModifContenu')}
-            <textarea id="contenu" value={mailManager.corpsMail} onChange={(evt) => { mailManager.corpsMail = evt.target.value; }} name="contenu" rows="8" className="block w-full p-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6" />
-          </label>
+          <h2 className="block text-sm sm:text-lg font-medium leading-6">
+            {t('courrielModifCorps')}
+          </h2>
+          <h3>
+            Desélectionnez les questions que vous ne voulez pas voir figurer dans le mail.
+          </h3>
+          <div className="grid">
+            <label>
+              <input type="checkbox" id="yearsAlt" name="yearsAlt" checked={textCheck[0]} onChange={() => { textCheck[0] = !textCheck[0]; changeBodyMail(); }} />
+              - Quelles années sont concernés par l&apos;alternance?
+            </label>
+            <label>
+              <input type="checkbox" id="planningAlt" name="planningAlt" checked={textCheck[1]} onChange={() => { textCheck[1] = !textCheck[1]; changeBodyMail(); }} />
+              - Quelles sont les plannings d&apos;alternance pour la rentrée prochaine?
+            </label>
+            <label>
+              <input type="checkbox" id="modalAlt" name="modalAlt" checked={textCheck[2]} onChange={() => { textCheck[2] = !textCheck[2]; changeBodyMail(); }} />
+              - Quelles sont les modalités pour gérer et suivre mon offre d&apos;alternance?
+            </label>
+            <label>
+              <input type="checkbox" id="adminAlt" name="adminAlt" checked={textCheck[3]} onChange={() => { textCheck[3] = !textCheck[3]; changeBodyMail(); }} />
+              - quelles sont les modalités administratives pour gérer ou suivre mon offre?
+            </label>
+          </div>
+          <div>
+            <label>
+              Si vous souhaitez demander des informations complémentaires :
+              <textarea id="contenu" onChange={(evt) => changeBodyMail(evt.target)} name="contenu" rows="2" className="block w-full p-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6" />
+            </label>
+          </div>
         </div>
 
         <div className="m-2">
           <h2 className="block text-sm sm:text-lg font-medium leading-6">{t('courrielModifOffre')}</h2>
           <label htmlFor="offre1">
             {t('courrielModifPropOffre1')}
+            <br />
+            <span className="italic">{t('courrielModifPropOffreWarning')}</span>
             <input
               type="file"
               onChange={(e) => {
